e61dab0ae4db0a6826003d27c2b95a89807ca06c
ertus0409 <ertus0409@hotmail.com>
2018-10-21 02:59:01 -0400

    Minor bug fixes

--- server/server.js
+++ server/server.js
@@ -21,24 +21,24 @@ app.use(bodyParser.json());
 // /answers
 app.post('/answers', (req, res) => {
 
-  const x0 = req.body.x0
-  const x1 = req.body.x1
-  const x2 = req.body.x2
-  const x3 = req.body.x3
-  const x4 = req.body.x4
-  const x5 = req.body.x5
-  const x6 = req.body.x6
+  console.log("POST /answers");
+  console.log();
+  console.log(req.body);
+
+  const x0 = req.body[0]
+  const x1 = req.body[1]
+  const x2 = req.body[2]
+  const x3 = req.body[3]
+  const x4 = req.body[4]
+  const x5 = req.body[5]
+  const x6 = req.body[6]
 
   const pyProg = spawn('python', ["./OTT/vienna.py",x0,x1,x2,x3,x4,x5,x6]);
 
   pyProg.stdout.on('data', function (data) {
-    if (data == null) {
-      return res.status(404).send()
-    }
     const result = data.toString();
     console.log(result);
-    res.status(200).send(result);
-
+    return res.status(200).send(result);
   });
 
 });
